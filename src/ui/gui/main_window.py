3956ca0ed636d9a1f54c241974582da067e8ce62
Victor Magela <victormateusamato@outlook.com>
2025-11-19 16:27:17 -0300

    Implementei conexão da janela principal da GUI com a camada de serviço.

--- src/ui/gui/main_window.py
+++ src/ui/gui/main_window.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QWidget, QMainWindow, QGridLayout, QPushButton, QT
 
 from src.ui.gui.table_model import TableModel
 from src.ui.gui.new_transaction_window import NewTransactionWindow
+from src.service.transaction_service import TransactionService
 from src.models.transaction import Transaction
 from src.models.enums import TransactionType, IncomeCategory, ExpenseCategory
 
@@ -11,53 +12,8 @@ from src.models.enums import TransactionType, IncomeCategory, ExpenseCategory
 class MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
-        self.fake_transactions: list[Transaction] = [
-    # Receitas
-    Transaction(
-        amount=3500.00,
-        transaction_type=TransactionType.INCOME,
-        transaction_date=date(2025, 10, 5),
-        category=IncomeCategory.WAGE,
-        description="Salário empresa X",
-    ),
-    Transaction(
-        amount=500.00,
-        transaction_type=TransactionType.INCOME,
-        transaction_date=date(2025, 10, 15),
-        category=IncomeCategory.FREELANCE,
-        description="Freelancer de desenvolvimento",
-    ),
+        self._service = TransactionService()
 
-    # Despesas
-    Transaction(
-        amount=120.75,
-        transaction_type=TransactionType.EXPENSE,
-        transaction_date=date(2025, 10, 8),
-        category=ExpenseCategory.FOOD,
-        description="Alimentação – almoço fora",
-    ),
-    Transaction(
-        amount=250.00,
-        transaction_type=TransactionType.EXPENSE,
-        transaction_date=date(2025, 10, 12),
-        category=ExpenseCategory.BILLS,
-        description="Conta de energia",
-    ),
-    Transaction(
-        amount=89.90,
-        transaction_type=TransactionType.EXPENSE,
-        transaction_date=date(2025, 11, 1),
-        category=ExpenseCategory.LEISURE,
-        description="Assinatura de serviços de streaming",
-    ),
-    Transaction(
-        amount=1500.00,
-        transaction_type=TransactionType.EXPENSE,
-        transaction_date=date(2025, 11, 5),
-        category=ExpenseCategory.HOUSING,
-        description="Aluguel",
-    ),
-]
         self.central_window = QWidget()
 
         self.grid_layout = QGridLayout()
@@ -79,26 +35,35 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.central_window)
         self.central_window.setLayout(self.grid_layout)
 
+        self._configure_layout()
+
+        self._configure_table()
+
+        self._configure_buttons()
+
+    def _configure_layout(self) -> None:
         self.grid_layout.addWidget(self.add_button, 0, 0, 1, 1)
         self.grid_layout.addWidget(self.edit_button, 0, 1, 1, 1)
         self.grid_layout.addWidget(self.filter_button, 0, 2, 1, 1)
         self.grid_layout.addWidget(self.report_button, 0, 3, 1, 1)
-        if self.fake_transactions:
+        if self._service.get_all_transactions():
             self.grid_layout.addWidget(self.table, 1, 0, 1, 4)
-            self.table_model.set_transaction_list(self.fake_transactions)
-            self._configure_table()
-            self.table.selectionModel().selectionChanged.connect(self._on_table_selection_changed)
-
+            self.table_model.set_transaction_list(self._service.get_all_transactions())
+        
         else:
             self.grid_layout.addWidget(self.no_table_label, 1, 0, 1, 4)
 
-        self._configure_buttons()
+        self.grid_layout.setRowStretch(0, 0)
+        self.grid_layout.setRowStretch(1, 1)
+        self.grid_layout.setColumnStretch(0, 0)
+        self.grid_layout.setColumnStretch(1, 1)
 
     def _configure_table(self) -> None:
         self.table.setModel(self.table_model)
         self.table.setSelectionBehavior(self.table.SelectionBehavior.SelectRows)
         self.table.setSelectionMode(self.table.SelectionMode.SingleSelection)
         self.table.setEditTriggers(self.table.EditTrigger.NoEditTriggers)
+        self.table.selectionModel().selectionChanged.connect(self._on_table_selection_changed)
 
     def _configure_buttons(self) -> None:
         self.edit_button.setEnabled(False)
@@ -113,8 +78,13 @@ class MainWindow(QMainWindow):
         new_transaction_window.exec()
         input_list = new_transaction_window.user_input_list
         for user_input in input_list:
-            print(f'Passando {user_input} ao service...')
-        print('janela de nova transação fechada...')
+            self._service.add_transaction(user_input)
+        
+        if input_list:
+            self.grid_layout.removeWidget(self.no_table_label)
+            self.no_table_label.hide()
+            self.table_model.set_transaction_list(self._service.get_all_transactions())
+            self.grid_layout.addWidget(self.table, 1, 0, 1, 4)
 
     def _edit_transaction(self) -> None:
         print('Editar transação')
@@ -125,5 +95,5 @@ class MainWindow(QMainWindow):
     def _generate_report(self) -> None:
         print('Gerar relatório')
 
-    def _on_table_selection_changed(self) -> None:
+    def _on_table_selection_changed(self, *_args) -> None:
         self.edit_button.setEnabled(self.table.selectionModel().hasSelection())
